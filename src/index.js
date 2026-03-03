c1c46197e69659f510c790c75872d0213a73a890
klanc0403 <kelly@kellyisasinger.com>
2026-03-03 16:03:06 -0500

    Injected the forecast HTML from JS

--- src/index.js
+++ src/index.js
@@ -53,5 +53,29 @@ function formatDate(date) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+    let forecastElement = document.querySelector("#forecast");
+
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    let forecastHtml = "";
+
+    days.forEach(function(day){
+        forecastHtml = forecastHtml +  `
+        <div class="weather-forecast-day"> 
+                        <div class="weather-forecast-date">${day}</div> 
+                        <div class="weather-forecast-icon">🌤️</div>
+                        <div class="weather-forecast-temperatures"> 
+                        <div class="weather-forecast-temperature">  <strong>32° </strong> </div> 
+                        <div class="weather-forecast-temperature"> 2° </div>
+                        </div>
+                        </div>
+`;
+    });
+
+    forecastElement.innerHTML = forecastHtml;
+}
+
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", search);
+
+displayForecast();
