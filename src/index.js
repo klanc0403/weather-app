c0147d00a9eca7ecec643e020b2db27c51dffd37
klanc0403 <kelly@kellyisasinger.com>
2026-03-03 14:23:40 -0500

    Fixed bug in time element

--- src/index.js
+++ src/index.js
@@ -3,9 +3,16 @@ function displayTemperature(response) {
   let temperature = Math.round(response.data.temperature.current);
   let cityElement = document.querySelector("#current-city");
   let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let date = new Date(response.data.time * 1000);
+  let timeElement = document.querySelector("#time");
   cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = temperature;
   descriptionElement.innerHTML = response.data.condition.description;
+  windElement.innerHTML = `${response.data.wind.speed}km/hr`;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  timeElement.innerHTML = formatDate(date);
 }
 
 function search(event) {
@@ -20,19 +27,7 @@ function search(event) {
 }
 
 function formatDate(date) {
-  let minutes = date.getMinutes();
-  let hours = date.getHours();
-  let day = date.getDay();
-
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
-  }
-
-  if (hours < 10) {
-    hours = `0${hours}`;
-  }
-
-  let days = [
+ let days = [
     "Sunday",
     "Monday",
     "Tuesday",
@@ -40,16 +35,20 @@ function formatDate(date) {
     "Thursday",
     "Friday",
     "Saturday",
-  ];
+  ]; 
+  
+  let day = days[date.getDay()];
+    let hours = date.getHours();
+  let minutes = date.getMinutes();
 
-  let formattedDay = days[day];
-  return `${formattedDay} ${hours}:${minutes}`;
+  if (minutes < 10) {
+    minutes = `0${minutes}`;}
+
+  if (hours < 10) {
+    hours = `0${hours}`;}
+
+  return `${day} ${hours}:${minutes}`;
 }
 
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", search);
-
-let currentDateElement = document.querySelector("#current-date");
-let currentDate = new Date();
-
-currentDateElement.innerHTML = formatDate(currentDate);
