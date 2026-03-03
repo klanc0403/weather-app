09ebb3db02edc87e3b32627facb09f8e835603f5
klanc0403 <kelly@kellyisasinger.com>
2026-03-03 13:27:34 -0500

    Updated API key

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ function search(event) {
   let searchInputElement = document.querySelector("#search-input");
   let city = searchInputElement.value;
 
-  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiKey = "0232oa2bd084ect6f17c5fee93b97744";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(displayTemperature);
